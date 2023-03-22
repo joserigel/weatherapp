6e921040eff64ad8120c51c55eb56e87cdbc9723
joserigel <joserigel18@gmail.com>
2023-03-22 11:36:55 +0100

    Removed useRef as it is not used

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 import './Search.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
